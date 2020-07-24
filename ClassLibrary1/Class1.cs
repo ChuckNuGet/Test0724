6c0bcaa8ef859976a2037a908dd34fa6badcfeb9
Chuck Lu <chuck.lu@qq.com>
2020-07-24 15:44:44 +0800

    initial version

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Guid = Guid.NewGuid();
+        }
+
+        public Guid Guid { get; }
+    }
+}
---
+++ ClassLibrary2/Class2.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary1;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public Class2()
+        {
+            Class1 = new Class1();
+        }
+
+        public Class1 Class1 { get; }
+
+        public  string GetGuid()
+        {
+            return Class1.Guid.ToString();
+        }
+    }
+}
