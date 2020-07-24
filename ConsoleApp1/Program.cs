d443c4767a4641156596530933253476afcde4fc
Chuck Lu <chuck.lu@qq.com>
2020-07-24 16:01:46 +0800

    test the published class library

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClassLibrary2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+                Class2 class2 = new Class2();
+                var guid = class2.GetGuid();
+                Console.WriteLine(guid);
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
